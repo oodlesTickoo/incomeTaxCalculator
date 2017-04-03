a2da5a9f73e8f11c3d989377bc64bebeebe40171
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2017-04-03 17:09:51 +0530

    clean up and final income tax calculator

--- js/controller.js
+++ js/controller.js
@@ -1,20 +1,20 @@
-app.controller("TTRController", ['$scope', '$timeout', 'TaxRateCalculator','ChartServiceHc', 'DonutChartServiceHc', 'PdfMaker', function($scope, $timeout, TaxRateCalculator, ChartServiceHc, DonutChartServiceHc, PdfMaker) {
+app.controller("TTRController", ['$scope', '$timeout', 'TaxRateCalculator', 'ChartServiceHc', 'DonutChartServiceHc', 'PdfMaker', function($scope, $timeout, TaxRateCalculator, ChartServiceHc, DonutChartServiceHc, PdfMaker) {
 
-    $scope.result={}
+    $scope.result = {}
 
     $scope.forms = {};
 
     $scope.personalDetails = {};
     $scope.personalDetails = {
-        firstName : "Amit",
-        lastName : "Kumar",
-        email : "iamitkrs@gmail.com",
+        firstName: "Amit",
+        lastName: "Kumar",
+        email: "iamitkrs@gmail.com",
         mobile: 412121212,
-        postalCode : 1234
+        postalCode: 1234
     };
 
     $scope.chartOneOpen = true;
-$scope.tempp;
+    $scope.tempp;
     String.prototype.replaceAll = function(search, replacement) {
         var target = this;
         return target.split(search).join(replacement);
@@ -33,18 +33,18 @@ $scope.tempp;
         { id: 1, name: "Fortnight" },
         { id: 2, name: "Month" }
     ];
-    var paymentFrequency="1";
+    var paymentFrequency = "1";
     $timeout(function() {
         $('.selectpicker').selectpicker({
             style: 'btn-info',
             size: 2,
         });
-        $('.selectpicker option[value="1"]').attr("selected",true);
+        $('.selectpicker option[value="1"]').attr("selected", true);
         $('.selectpicker').selectpicker('refresh');
     });
 
     $('.selectpicker').on('change', function() {
-        paymentFrequency= $('.selectpicker option:selected').val();
+        paymentFrequency = $('.selectpicker option:selected').val();
         // calculateFinal();
         $timeout(0);
     });
@@ -67,12 +67,12 @@ $scope.tempp;
         connect: 'lower'
     });
     var annualSalaryInput = document.getElementById('annualSalaryInput');
-    
+
     annualSalarySlider.noUiSlider.on('update', function(values, handle) {
         annualSalaryInput.value = values[handle];
         $scope.annualSalary = (values[handle]);
     });
-    
+
     annualSalaryInput.addEventListener("change", function() {
         annualSalarySlider.noUiSlider.set($scope.annualSalary);
     });
@@ -81,93 +81,98 @@ $scope.tempp;
         // calculateFinal();
         $timeout(0);
     });
-    
+
     // annualSalarySlider.noUiSlider.on('set', function(values, handle) {
     //     annualSalaryInput.value = values[handle];
     //     $scope.annualSalary = (values[handle]);
     // });
-    
-    $scope.calculateFinal = function(isValid){
-        if(isValid){
-        var salary = Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', ''));
-        var taxOnIncome = TaxRateCalculator.getTaxBase(salary) +
-            (salary - TaxRateCalculator.getLowerBoundValue(salary) + 1) * TaxRateCalculator.getTaxRate(salary);
-        var netAnnualIncomeAfterTax = salary - taxOnIncome;
-        var netPaymentPerPeriod = netAnnualIncomeAfterTax / (TaxRateCalculator.getPeriods(paymentFrequency));
 
-        $scope.result.taxOnIncome=taxOnIncome;
-        $scope.result.netAnnualIncomeAfterTax=netAnnualIncomeAfterTax;
-        $scope.result.netPaymentPerPeriod=netPaymentPerPeriod;
+    $scope.calculateFinal = function(isValid) {
+        if (isValid) {
+            var salary = Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', ''));
+            var taxOnIncome = TaxRateCalculator.getTaxBase(salary) +
+                (salary - TaxRateCalculator.getLowerBoundValue(salary) + 1) * TaxRateCalculator.getTaxRate(salary);
+            var netAnnualIncomeAfterTax = salary - taxOnIncome;
+            var netPaymentPerPeriod = netAnnualIncomeAfterTax / (TaxRateCalculator.getPeriods(paymentFrequency));
 
-        switch(paymentFrequency){
-            case "0": $scope.result.paymentFrequency="Week";break;
-            case "1":$scope.result.paymentFrequency="Fortnight";break;
-            case "2":$scope.result.paymentFrequency="Month";break;
+            $scope.result.taxOnIncome = taxOnIncome;
+            $scope.result.netAnnualIncomeAfterTax = netAnnualIncomeAfterTax;
+            $scope.result.netPaymentPerPeriod = netPaymentPerPeriod;
 
+            switch (paymentFrequency) {
+                case "0":
+                    $scope.result.paymentFrequency = "Week";
+                    break;
+                case "1":
+                    $scope.result.paymentFrequency = "Fortnight";
+                    break;
+                case "2":
+                    $scope.result.paymentFrequency = "Month";
+                    break;
+
+            }
+
+            // console.log("1.",taxOnIncome);
+            // console.log("2.",netAnnualIncomeAfterTax);
+            // console.log("3.",netPaymentPerPeriod);
+
+            ChartServiceHc.createChart(Number(taxOnIncome.toFixed(2)), Number(netAnnualIncomeAfterTax.toFixed(2)), false);
+            DonutChartServiceHc.createChart(Number(taxOnIncome.toFixed(2)), Number(netAnnualIncomeAfterTax.toFixed(2)));
+            //PdfMaker.createChart($scope.personalDetails,Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', '')),$scope.result);
+
+
+        } else {
+            $("#myModal").modal('show');
+            $("html, body").animate({ scrollTop: 0 }, "slow");
         }
-
-        // console.log("1.",taxOnIncome);
-        // console.log("2.",netAnnualIncomeAfterTax);
-        // console.log("3.",netPaymentPerPeriod);
-
-        ChartServiceHc.createChart(Number(taxOnIncome.toFixed(2)), Number(netAnnualIncomeAfterTax.toFixed(2)), false);
-        DonutChartServiceHc.createChart( Number(taxOnIncome.toFixed(2)), Number(netAnnualIncomeAfterTax.toFixed(2)) );
-        //PdfMaker.createChart($scope.personalDetails,Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', '')),$scope.result);
-        document.getElementById("ymm").style.display="none";
-        document.getElementById("ymm2").style.display="block";
-        
-    }else{
-               $("#myModal").modal('show');
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    }
     }
 
     $scope.calculateFinal(true);
 
-    document.getElementById("download").addEventListener("click",function(){
-        if($scope.forms.ttrForm.$valid){
-        PdfMaker.createChart($scope.personalDetails,Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', '')),$scope.result);
-    }else{
-        $("#myModal").modal('show');
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-    }
+    document.getElementById("download").addEventListener("click", function() {
+        if ($scope.forms.ttrForm.$valid) {
+            PdfMaker.createChart($scope.personalDetails, Number($scope.annualSalary.replaceAll('$', '').replaceAll(',', '')), $scope.result);
+        } else {
+            $("#myModal").modal('show');
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }
     });
 
-    document.getElementById("bar-chart").addEventListener("click",function(){
+    document.getElementById("bar-chart").addEventListener("click", function() {
         $scope.chartOneOpen = true;
         document.getElementById("donutContainer").style.display = "none";
         document.getElementById("container").style.display = "block";
     });
 
-    document.getElementById("donut-chart").addEventListener("click",function(){
+    document.getElementById("donut-chart").addEventListener("click", function() {
         $scope.chartOneOpen = false;
         document.getElementById("container").style.display = "none";
         document.getElementById("donutContainer").style.display = "block";
     });
 
-    $(".print-doc").on("click",printBothCharts);
+    $(".print-doc").on("click", printBothCharts);
 
-    function printBothCharts(){
+    function printBothCharts() {
 
-        if($scope.forms.ttrForm.$valid){
+        if ($scope.forms.ttrForm.$valid) {
 
-            if($scope.chartOneOpen){
-        document.getElementById("donutContainer").style.display = "block";
-           window.print();
-           setTimeout(function(){
-            document.getElementById("donutContainer").style.display = "none";
-         },100);
-       }else{
-        document.getElementById("container").style.display = "block";
-           window.print();
-           setTimeout(function(){
-            document.getElementById("container").style.display = "none";
-         },100);
-       }    
-   }else{
-        $("#myModal").modal('show');
-        $("html, body").animate({ scrollTop: 0 }, "slow");    
-   }
-}
+            if ($scope.chartOneOpen) {
+                document.getElementById("donutContainer").style.display = "block";
+                window.print();
+                setTimeout(function() {
+                    document.getElementById("donutContainer").style.display = "none";
+                }, 100);
+            } else {
+                document.getElementById("container").style.display = "block";
+                window.print();
+                setTimeout(function() {
+                    document.getElementById("container").style.display = "none";
+                }, 100);
+            }
+        } else {
+            $("#myModal").modal('show');
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        }
+    }
 
 }]);
